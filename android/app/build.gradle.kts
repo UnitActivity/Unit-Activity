63a58a31fa6092daf1b3c2041e39297dffd93dfd
Mikaela <giokpheng100@gmail.com>
2026-01-04 13:40:54 +0700

    build: Add Android and Windows platform files and update package dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.unit_activity"
+    namespace = "id.unit.activity"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.unit_activity"
+        applicationId = "id.unit.activity"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
